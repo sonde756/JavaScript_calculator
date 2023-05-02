b4a79c12cf9566e1a2a74231938571d8f4f48b83
sonde756 <edsonrafael756@gmail.com>
2023-05-01 21:58:20 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let friends = ['Marcos', 'Ana', 'Joel']
+
+
+const movies = [
+  {id: 1, movieName: "Filme1"},
+  {id: 2, movieName: "Filme2"},
+  {id: 3, movieName: "Filme3"}
+]
+
+console.log(movies.find(movie => movie.movieName == "Filme1"))
